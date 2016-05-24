77481e266e87a1449e733168acea6e7bea8e741d
Dominik Figl <dominik.figl@technikum-wien.at>
2016-05-24 20:28:19 +0200

    90 Grad done

--- kernel.cl
+++ kernel.cl
@@ -21,3 +21,38 @@ __kernel void vector_add(
 	int i = get_global_id(0);
 	C[i] = A[i] + B[i];
 }
+
+__kernel void image_rotate( 
+	__global unsigned char * src_data, __global unsigned char * dest_data, 	//Data in global memory
+	int W, int H,  							//Image Dimensions 
+	float sinTheta, float cosTheta ) 				//Rotation Parameters 
+	{ 
+	
+		//Thread gets its index within index space 
+		const int ix = get_global_id(0); 
+		const int iy = get_global_id(1); 
+		const int centerX = (int) (W/2);
+		const int centerY = (int) (H/2);
+		
+		//Calculate location of data to move into ix and iy– Output decomposition as mentioned 
+		float xpos = (( ((float) ix - (float)centerX)*cosTheta + ((float)iy - (float) centerY)*sinTheta) + centerX); 
+		float ypos = (( ((float) ix - (float)centerX)*sinTheta + ((float)iy - (float) centerY)*cosTheta) + centerY);
+
+		if ((((int)xpos>=0) && ((int)xpos< W)) && ( ((int)ypos>=0) && ((int)ypos< H)) )
+		{
+			//Read (xpos,ypos) src_data and store at (ix,iy) in dest_data 
+			int index = (iy*W+ix)*3; 
+			int sourceIndex = (int)(floor((ypos*W+xpos)*3));
+			dest_data[index+0]= src_data[sourceIndex+0]; 
+			dest_data[index+1]= src_data[sourceIndex+1]; 
+			dest_data[index+2]= src_data[sourceIndex+2]; 
+		}
+		else
+		{
+			dest_data[(iy*W+ix)*3+0]= 255; 
+			dest_data[(iy*W+ix)*3+1]= 0;
+			dest_data[(iy*W+ix)*3+2]= 0;
+		}	
+		
+	} 
+
--- main.cpp
+++ main.cpp
@@ -19,6 +19,71 @@
 #include <iostream>
 #include <fstream>
 
+
+int mainASDF(void)
+{
+
+#if defined(__APPLE__) || defined(__MACOSX)
+	const std::string KERNEL_FILE = "./kernel.cl";
+	int platformNr = 0;
+#else
+	const std::string KERNEL_FILE = "kernel.cl";
+	int platformNr = 1;
+#endif
+
+
+	//http://simpleopencl.blogspot.co.at/2013/06/tutorial-simple-start-with-opencl-and-c.html
+	//get all platforms (drivers)
+	std::vector<cl::Platform> all_platforms;
+	cl::Platform::get(&all_platforms);
+	if (all_platforms.size() == 0){
+		std::cout << " No platforms found. Check OpenCL installation!\n";
+		exit(1);
+	}
+	cl::Platform default_platform = all_platforms[0];
+	std::cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+
+	//get default device of the default platform
+	std::vector<cl::Device> all_devices;
+	default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+	if (all_devices.size() == 0){
+		std::cout << " No devices found. Check OpenCL installation!\n";
+		exit(1);
+	}
+	cl::Device default_device = all_devices[platformNr];
+	std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
+
+	cl::Context context({ default_device });
+
+	cl::Program::Sources sources;
+
+	// load and build the kernel
+	std::ifstream sourceFile(KERNEL_FILE);
+	std::string sourceCode(
+		std::istreambuf_iterator<char>(sourceFile),
+		(std::istreambuf_iterator<char>()));
+	cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
+	cl::Program program(context, sources);
+
+	if (program.build({ default_device }) != CL_SUCCESS){
+		std::cout << " Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device) << "\n";
+		exit(1);
+	}
+
+	tga::TGAImage* img = new tga::TGAImage();
+	tga::LoadTGA(img, "lenna.tga");
+
+	size_t _size = img->imageData.size() * sizeof(unsigned char);
+
+	cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, _size);
+	cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, _size);
+
+	//cl::make_kernel test(cl::Kernel(program, "simple_add"));
+	//cl::EnqueueArgs eargs(queue, cl::NullRange, cl::NDRange(10), cl::NullRange);
+	//simple_add(eargs, buffer_A, buffer_B, buffer_C).wait();
+}
+	
+
 int  main(void)
 {
 
@@ -29,6 +94,7 @@ int  main(void)
 	const std::string KERNEL_FILE = "kernel.cl";
 	int platformNr = 1;
 #endif
+
 	cl_int err = CL_SUCCESS;
 	cl::Program program;
 	std::vector<cl::Device> devices;
@@ -67,63 +133,130 @@ int  main(void)
 			(std::istreambuf_iterator<char>()));
 		cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
 		program = cl::Program(context, source);
-		program.build(devices);
+		//program.build(devices);
+
+		if (program.build({ devices }) != CL_SUCCESS){
+			//std::cout << " Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices) << "\n";
+			exit(1);
+		}
+
 		//create kernels
-		cl::Kernel kernel(program, "hello", &err);
+		cl::Kernel kernel(program, "image_rotate", &err);
 		cl::Event event;
 
-		// launch hello kernel
-		std::cout << "calling 'hello' kernel" << std::endl;
 		cl::CommandQueue queue(context, devices[0], 0, &err);
-		queue.enqueueNDRangeKernel(
-			kernel,
-			cl::NullRange,
-			cl::NDRange(4, 4),
-			cl::NullRange,
-			NULL,
-			&event);
-		event.wait();
 
 		// create input and output data
-		std::vector<int> a({ 1, 2, 3, 4, 5, 6, 7, 8 });
-		std::vector<int> b({ 1, 2, 3, 4, 5, 6, 7, 8 });
-		std::vector<int> c;
-		c.resize(a.size());
+		tga::TGAImage* imgDest = new tga::TGAImage();
+		imgDest->bpp = img->bpp;
+		imgDest->height = img->height;
+		imgDest->width= img->width;
+		imgDest->type = img->type;
+		imgDest->imageData.resize(img->imageData.size());
+
+		size_t _size = img->imageData.size() * sizeof(unsigned char);
+
 		// input buffers
-		cl::Buffer bufferA = cl::Buffer(context, CL_MEM_READ_ONLY, a.size() * sizeof(int));
-		cl::Buffer bufferB = cl::Buffer(context, CL_MEM_READ_ONLY, b.size() * sizeof(int));
+		cl::Buffer bufferSource = cl::Buffer(context, CL_MEM_READ_ONLY, _size);
 		// output buffers
-		cl::Buffer bufferC = cl::Buffer(context, CL_MEM_WRITE_ONLY, c.size() * sizeof(int));
+		cl::Buffer bufferDest = cl::Buffer(context, CL_MEM_WRITE_ONLY, _size);
+
+		cl_int clWidth = img->width;
+		cl_int clHeight = img->height;
+
+		float sintheta = 1;
+		float costheta = 0;
 
 		// fill buffers
 		queue.enqueueWriteBuffer(
-			bufferA, // which buffer to write to
+			bufferSource, // which buffer to write to
 			CL_TRUE, // block until command is complete
 			0, // offset
-			a.size() * sizeof(int), // size of write 
-			&a[0]); // pointer to input
-		queue.enqueueWriteBuffer(bufferB, CL_TRUE, 0, b.size() * sizeof(int), &b[0]);
+			_size, // size of write 
+			&(img->imageData[0])); // pointer to input
 
-		cl::Kernel addKernel(program, "vector_add", &err);
-		addKernel.setArg(0, bufferA);
-		addKernel.setArg(1, bufferB);
-		addKernel.setArg(2, bufferC);
-
+		cl::Kernel rotateKernel(program, "image_rotate", &err);
+		rotateKernel.setArg(0, bufferSource);
+		rotateKernel.setArg(1, bufferDest);
+		rotateKernel.setArg(2, img->width);
+		rotateKernel.setArg(3, img->height);
+		rotateKernel.setArg(4, sintheta);
+		rotateKernel.setArg(5, costheta);
+				
 		// launch add kernel
 		// Run the kernel on specific ND range
-		cl::NDRange global(a.size());
-		cl::NDRange local(1); //make sure local range is divisible by global range
-		cl::NDRange offset(0);
-		std::cout << "call 'vector_add' kernel" << std::endl;
-		queue.enqueueNDRangeKernel(addKernel, offset, global, local);
+		cl::NDRange global(img->width, img->height);
+		cl::NDRange local(1,1); //make sure local range is divisible by global range
+		cl::NDRange offset(0,0);
+		std::cout << "call 'image_rotate' kernel" << std::endl;
+		queue.enqueueNDRangeKernel(rotateKernel, offset, global, local);
+		//event.wait();
 
 		// read back result
-		queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, c.size() * sizeof(int), &c[0]);
+		//queue.enqueueReadBuffer(bufferDest, CL_TRUE, 0, _size, &c[0]);
+		queue.enqueueReadBuffer(bufferDest, CL_TRUE, 0, _size, &(imgDest->imageData[0]));
 
-		for (int i : c)
-		{
-			std::cout << i << std::endl;
-		}
+		tga::saveTGA(*imgDest, "test.tga");
+
+		////////////////////////////////////////////////////////////////////////////////////////
+
+
+		////create kernels
+		//cl::Kernel kernel(program, "hello", &err);
+		//cl::Event event;
+
+		//// launch hello kernel
+		//std::cout << "calling 'hello' kernel" << std::endl;
+		//cl::CommandQueue queue(context, devices[0], 0, &err);
+		//queue.enqueueNDRangeKernel(
+		//	kernel,
+		//	cl::NullRange,
+		//	cl::NDRange(4, 4),
+		//	cl::NullRange,
+		//	NULL,
+		//	&event);
+		//event.wait();
+
+		//// create input and output data
+		//std::vector<int> a({ 1, 2, 3, 4, 5, 6, 7, 8 });
+		//std::vector<int> b({ 1, 2, 3, 4, 5, 6, 7, 8 });
+		//std::vector<int> c;
+		//c.resize(a.size());
+		//// input buffers
+		//cl::Buffer bufferA = cl::Buffer(context, CL_MEM_READ_ONLY, a.size() * sizeof(int));
+		//cl::Buffer bufferB = cl::Buffer(context, CL_MEM_READ_ONLY, b.size() * sizeof(int));
+		//// output buffers
+		//cl::Buffer bufferC = cl::Buffer(context, CL_MEM_WRITE_ONLY, c.size() * sizeof(int));
+
+		//// fill buffers
+		//queue.enqueueWriteBuffer(
+		//	bufferA, // which buffer to write to
+		//	CL_TRUE, // block until command is complete
+		//	0, // offset
+		//	a.size() * sizeof(int), // size of write 
+		//	&a[0]); // pointer to input
+		//queue.enqueueWriteBuffer(bufferB, CL_TRUE, 0, b.size() * sizeof(int), &b[0]);
+
+		//cl::Kernel addKernel(program, "vector_add", &err);
+		//addKernel.setArg(0, bufferA);
+		//addKernel.setArg(1, bufferB);
+		//addKernel.setArg(2, bufferC);
+
+		//// launch add kernel
+		//// Run the kernel on specific ND range
+		//cl::NDRange global(a.size());
+		//cl::NDRange local(1); //make sure local range is divisible by global range
+		//cl::NDRange offset(0);
+		//std::cout << "call 'vector_add' kernel" << std::endl;
+		//queue.enqueueNDRangeKernel(addKernel, offset, global, local);
+
+		//// read back result
+		//queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, c.size() * sizeof(int), &c[0]);
+
+		//for (int i : c)
+		//{
+		//	std::cout << i << std::endl;
+		//}
 
 	}
 	catch (cl::Error err) {
